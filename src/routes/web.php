25d0aae126c80df01eb6b484167c65cfe1d99a18
dump14040161 <itce@PC0069.local>
2019-07-12 16:49:25 +0900

    Taskを追加できるようにした

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Task;
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +16,23 @@
 
 Route::get('/', function () {
     return view('tasks');
+});
+
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task();
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+    
 });
